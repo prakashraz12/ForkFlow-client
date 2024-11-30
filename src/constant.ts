23fdedec5064fb3f5718e555c9cc96796d2f0944
Prakash Shrestha <rzprakash16@gmail.com>
2024-11-30 09:58:08 +0545

    fit-project-set-up

---
+++ src/constant.ts
@@ -0,0 +1,6 @@
+export const MAIN_COLOR = '#50C67C';
+export const MAIN_COLOR_DARK = '#31A497';
+export const SECONDARY_COLOR = '#F7B11D';
+export const SECONDARY_COLOR_DARK = '#E9880F';
+export const TEXT_COLOR = '#193648';
+export const TEXT_COLOR_DARK = '#000000';
